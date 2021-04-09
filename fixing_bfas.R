2c119570a3e4ee1ede74edab75b00cbfcc3f2ade
sheths3 <sheths3@upmc.edu>
2021-04-09 16:45:50 -0400

    updates

--- fixing_bfas.R
+++ fixing_bfas.R
@@ -3,9 +3,9 @@
 ### Make a function to reverse the scores: (max score + 1) - (score)
 fix_bfas <- function(x) (6-x)
 
-### use this function  with mutate for all variables that start with "bfas" (to exclude the ID number and other things)
+### Use this function  with mutate for all the item variables (those that start with "bfas") (to exclude the ID number and other things)
 bfas_fixed <- bfas %>% mutate_at(vars(starts_with("bfas")), fix_bfas)
                   
-### write into a csv separate from the old (unfixed) data
+### Write into a csv separate from the old (unfixed) data
 write.csv(bfas_fixed, "/Users/shreya/Box/skinner/data/PANDA/Psychosocial/bfas_raw_scores_fixed.csv", row.names = FALSE)
 
