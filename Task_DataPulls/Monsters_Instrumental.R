19be861755f0a185b8b41fbe34df7e5ad100ecab
Timothy Allen <timothy.a.allen@gmail.com>
2021-04-16 23:59:40 -0400

    Monsters_PIT_import

--- Task_DataPulls/Monsters_Instrumental.R
+++ Task_DataPulls/Monsters_Instrumental.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 library(R.matlab)
 
-setwd("~/Box/skinner/data/PANDA/Monsters")
+#os <- "/Users/timallen/Box/skinner/data/PANDA/Monsters/"
+os <- "C:/Users/timot/Box/skinner/data/PANDA/Monsters/"
+setwd(paste0(os))
 
-mat_ids <- as.numeric(list.files(path = "/Users/timallen/Box/skinner/data/PANDA/Monsters", recursive=FALSE)) #grab the list of ids
 mat_files <- list.files(pattern="*.ProcessedData.mat", recursive=TRUE)
 
 out = list()
 for (i in mat_files) {
-  x <- readMat(paste0("/Users/timallen/Box/skinner/data/PANDA/Monsters/",
+  x <- readMat(paste0(os,
                       i))
   subdata <- data.frame(reward = unlist(x$Data[[1]][1]), #whether a reward or punishmnet was given
                         response = unlist(x$Data[[1]][2]), # number of presses made
@@ -25,4 +26,5 @@ for (i in mat_files) {
 ins_data <- bind_rows(out)
 
 
-#presstimest = times of the button presses relative to the onset of the instrumental stimulus
+sub1 <- readMat(paste0(os, "440269/PIT_440269_1_210312_0959_ProcessedData.mat"))
+sub1$Data[[1]]
---
+++ Task_DataPulls/Monsters_PIT.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(R.matlab)
+
+
+#os <- "/Users/timallen/Box/skinner/data/PANDA/Monsters/"
+os <- "C:/Users/timot/Box/skinner/data/PANDA/Monsters/"
+setwd(paste0(os))
+
+mat_files <- list.files(pattern="*.ProcessedData.mat", recursive=TRUE)
+
+#remove 440269, because she only seems to have 131 responses for some reason
+mat_files <- mat_files[!mat_files %in% "440269/PIT_440269_1_210312_0959_ProcessedData.mat"]
+
+out = list()
+for (i in mat_files) {
+  x <- readMat(paste0(os, i))
+  
+  subdata <- data.frame(responses = unlist(x$Data[[2]][1]), #number of presses made
+                        ins_stim = unlist(x$Data[[2]][2]), # which ins stim
+                        correct = unlist(x$Data[[2]][3]), # was it the correct response?
+                        pav_stim = unlist(x$Data[[2]][4]), # pav stim
+                        pav_val = unlist(x$Data[[2]][5]), # value of the pav stimulus
+                        rt_first = unlist(x$Data[[2]][6]), #rt for first response
+                        instr_resp = unlist(x$Data[[2]][8])) #correct response for each trial
+  
+  subdata$id <- as.numeric(sub(".*/PIT_(\\d+)_(\\d)_(\\d+)_(\\d+)_ProcessedData.mat", "\\1", i, perl=TRUE)) 
+  
+  
+  out[[i]] <-  subdata
+}
+
+pit_data <- bind_rows(out)
